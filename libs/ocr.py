5caa1d2d939cb8901d13a975db5e662575882d69
Pallavi Pundir <pallavi.pundir@biz2credit.org>
2017-08-24 16:23:50 +0530

    my commit

--- libs/ocr.py
+++ libs/ocr.py
@@ -43,6 +43,6 @@ def ocr_default(img_file,preprocess) :
     # show the output images
     # cv2.imshow("Image", image)
     # cv2.imshow("Output", gray)
-    cv2.waitKey(0)
+    #cv2.waitKey(0)
 
     return finaltext
