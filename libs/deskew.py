890323ef7352f61bb0bc780bb046d05f1763ef63
Pallavi Pundir <pallavi.pundir@biz2credit.org>
2017-09-13 18:44:38 +0530

    ab

--- libs/deskew.py
+++ libs/deskew.py
@@ -1,3 +1,6 @@
+import cv2
+import numpy as np
+
 contours, hierarchy = cv2.findContours(img_dilate.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
 points=[]
--- libs/test.py
+++ libs/test.py
@@ -20,11 +20,26 @@ def ocr_default(img_file,preprocess) :
     # Load image, resize width and height & then convert to grayscale
     image = cv2.imread(img_file,0)
     height, width = image.shape[:2]
-    image = cv2.resize(image, (width*1, height*1), interpolation = cv2.INTER_AREA) 
+    image = cv2.resize(image, (width*2, height*2), interpolation = cv2.INTER_CUBIC) 
+    #resized_image = cv2.resize(image, (, 100))
+    #rows,cols = image.shape
+    #M = cv2.getRotationMatrix2D((width/2,height/2),360,1)
+    #dst = cv2.warpAffine(image,M,(width,height))
+    #equ = cv2.equalizeHist(image)
+    cv2.imwrite("E:/licence/10.jpg",image)
+    thresh = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1] 
+    cv2.imwrite("E:/licence/1.jpg",thresh)
+    # edge detection
+    #edges = cv2.Canny(thresh,2,100, apertureSize = 3)
+    #cv2.imwrite("E:/licence/2.jpg",edges)
+    # fill the holes from detected edges
+    kernel = np.ones((2,2),np.uint8)
+    dilate = cv2.dilate(thresh, kernel, iterations=1)
+    cv2.imwrite("E:/licence/3.jpg",dilate)
     #enh = ImageEnhance.Contrast(image)
     #cv2.imwrite("E:/licence/img7.jpg",enh)
     #gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
+    
     # check to see if we should apply thresholding to preprocess the image #
     #if preprocess == "thresh":
     #gray = cv2.threshold(gray, 0, 255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
@@ -34,7 +49,7 @@ def ocr_default(img_file,preprocess) :
     #elif preprocess == "blur":
      #   gray = cv2.m edianBlur(gray, 3)
     
-    dst = cv2.fastNlMeansDenoising(image,None,10,7,21)
+    dst = cv2.fastNlMeansDenoising(dilate,None,10,7,21)
     dst1 = cv2.fastNlMeansDenoising(dst,None,7,5,17)
     dst2 = cv2.fastNlMeansDenoising(dst1,None,7,5,17)
    # b,g,r = cv2.split(dst2)           # get b,g,r
@@ -45,14 +60,14 @@ def ocr_default(img_file,preprocess) :
     boxFilter=np.ones((5,5),np.float32)/91.0 # default is 81.0 Blurs an image using the box filter.
     kernel=kernel-boxFilter
     custom=cv2.filter2D(dst2,-1,kernel)
-    #dilation = cv2.dilate(custom,kernel,iterations =1)
-    #cv2.imwrite("E:/licence/img8.jpg",dilation)
+    dilation = cv2.dilate(custom,kernel,iterations =1)
+   # cv2.imwrite("E:/licence/img8.jpg",custom)
     
     #equ = cv2.equalizeHist(custom)
     #cv2.imwrite("E:/licence/img.jpg",custom)
     
     # write the grayscale image to disk as a temporary file so we can apply OCR to it #
-    filename = "E:/OCR_Work/ocr_opencv/storage/Image/files/{}.png".format(os.getpid())
+    filename = "E:/OCR_Work/ocr_opencv/storage/files/Image/{}.png".format(os.getpid())
     cv2.imwrite(filename, custom)
     cv2.imwrite("E:/licence/img1.png",custom)
 
@@ -62,7 +77,7 @@ def ocr_default(img_file,preprocess) :
     #text=re.match(r"[a-zA-Z\s*]",text)
     #text=re.match(r"[a-zA-Z\s*a-zA-Z\s*a-zA-Z\s*0-9a-zA-Z\s*(DOB|date\s*of\s*birth)\s*:?\s*[0-9]{2}\s*\/\s*[0-9]{1,2}\s*\/\s*[0-9]{4}a-zA-Z\s*a-zA-Z\s*a-zA-Z\s*[0-9]{1}\s*\-[0-9]{1}\s*[0-9]\s*a-zA-Z\s*[0-9]{2}\s*\-[0-9]{4}\s*a-zA-Z\s*[0-9]{2}\s*\-\s*[0-9]{1,2}\s*\-\s*[0-9]{4}",text)
     text = re.sub('[^a-zA-Z0-9\s\d\w\b\t\n:/\-\'^.,*\/]*','', text)
-    text1=text.split()
+    #text1=text.split()
     #print text1
     #print("length is",len(text1))
     #For appending or store the results 
@@ -89,7 +104,7 @@ def ocr_default(img_file,preprocess) :
 
     #output_join = [text1[0],"\n","Texas","\n ",text1[3],text1[4],"NO. :",text1[12],"\n",text1[14],text1[15],"\nIssue Date:",text1[18],"\nExpiry Date:",text1[21],"\nDate of Birth:",text1[24],"\nName:",text1[25],"\nFather's Name:",text1[26],text1[27],"\nAddress:",text1[29],text1[30],text1[31],",","\n",text1[32],text1[33],text1[34]]
     #output_join = [text1[0],"\n","Texas","\n ",text1[2],text1[3],",",text1[4]," ",text1[5],":",text1[6],"\n",text1[7]," ",text1[8],text1[9],"\nIssue Date:",text1[12],"\nExpiry Date:",text1[11]," ",text1[12]," ",text1[13]," ",text1[14]," ",text1[15],"\nDate of Birth:",text1[16]," ",text1[17]," ",text1[18],"\nName:",text1[20],"\nFather's Name:",text1[21],text1[22],"\nAddress:",text1[24],text1[25],",","\n",text1[26],text1[27],text1[28],text1[29]] 
-    output_join = [text1[0],"\n",text1[1],text1[2],text1[3],text1[4],text1[5],text1[6],"\n",text1[7],text1[8],text1[9],text1[10],text1[11]," ::",text1[12]," ",text1[13]," ",text1[14]," ",text1[15],text1[16]," ",text1[17]," ",text1[18],text1[19],text1[20],text1[21],text1[22],text1[23],text1[24],text1[25],"\n",text1[26],text1[27],text1[28],text1[29]] 
+    #output_join = [text1[0],"\n",text1[1],text1[2],"\nDL:",text1[5],"\n",text1[6],"\n",text1[7],text1[8],text1[9],text1[10],text1[11],text1[12],text1[13],text1[14],text1[15],text1[16],text1[17],text1[18]] 
     #for elem in output_join:
      #   print elem 
     #print "\n".join(output_join)
@@ -147,5 +162,5 @@ def ocr_default(img_file,preprocess) :
     # cv2.imshow("Output", gray)
     #cv2.waitKey(0)
     
-    return " ".join(output_join) #.strip('"\'')
-    #return finaltext
+    #return " ".join(output_join) #.strip('"\'')
+    return finaltext
