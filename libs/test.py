afda633cb75c4fc817d8f38c7aad39b71616cdf4
Pallavi Pundir <pallavi.pundir@biz2credit.org>
2017-09-27 17:36:18 +0530

    pt

--- libs/test.py
+++ libs/test.py
@@ -9,7 +9,7 @@ def ocr_default(img_file, preprocess) :
     from PIL import Image
     import pytesseract
     import cv2
-    import os , re
+    import os , re, regex
     import numpy as np
     from datetime import datetime
     import dateutil
@@ -93,7 +93,7 @@ def ocr_default(img_file, preprocess) :
     #return newtest
     #print("length is",len(text1))
     #For appending or store the results 
- 
+    #return newtest
 
     
     #output_join = [text1[0],"Texas",text1[1],text1[2],text1[3],text1[4],text1[5],text1[6],text1[7],text1[8],text1[9],text1[10],text1[11],text1[12],text1[13],text1[14],text1[15],text1[16],text1[17],text1[18]] 
@@ -131,14 +131,14 @@ def ocr_default(img_file, preprocess) :
     #return text
     #print np.array(list(text))
     regexArray = {}
-    regexArray['DL'] = r'DL\s*[0-9]{8,10}\s*(?=[0-9]+\s*.*)'
-    regexArray['CLASS']=r'CLASS+[a-zA-Z]'
-    regexArray['ISS']=r'ISS\s+[0-9]{2}(\-|\/)[0-9]{2}(\-|\/)[0-9]{4}'
-    regexArray['EXP']=r'EXP\s+[0-9]{2}(\-|\/)[0-9]{2}(\-|\/)[0-9]{4}'
-    regexArray['DOB']=r'DOB\s*[0-9]{2}(\-|\/)[0-9]{2}(\-|\/)[0-9]{4}'
-    regexArray['NAME']=r'[0-9]{2}(\-|\/)[0-9]{2}(\-|\/)[0-9]{4}\s*[0-9]{0,2}\s*[A-Z]+\s*[0-9]{0,2}\s*[A-Z\s]+(?=\s*[0-9]{1,2}\s*[0-9]{2,5})'
+    regexArray['DL'] = r'DL\s*\K[0-9]{8,10}\s*(?=[0-9]+\s*.*)|NUMBER\s*\K[A-Z0-9]{8,11}(?=\s*.*)'
+    regexArray['CLASS']=r'CLASS\s*\K[A-Z](?=.*)'
+    regexArray['ISS']=r'(ISSUED|(I|L)SS)\s*\K[0-9]{2}(\-|\/)[0-9]{2}(\-|\/)[0-9]{4}'
+    regexArray['EXP']=r'(EXP|EXPIRES)\s*\K[0-9]{2}(\-|\/)[0-9]{2}(\-|\/)[0-9]{4}'
+    regexArray['DOB']=r'(DOB|DATE\*OF\*BIRTH|ONE)\s*\K[0-9]{2}(\-|\/)[0-9]{2}(\-|\/)[0-9]{4}'
+    regexArray['NAME']=r'(DOB|ONE)\s*[0-9]{2}\/[0-9]{2}\/[0-9]{4}\s*([0-9]{1,2})?\s*\K[A-Z\s]+([0-9]{1,2})?\s*[A-Z\s]+|(DOB|ONE)\s*[0-9]{2}\-[0-9]{2}\-[0-9]{4}\s*([0-9]{1,2})?\s*\K[A-Z\s]+([0-9]{1,2})?\s*[A-Z\s]+|DRIVER\s*LICENSE\s*\K[A-Z\s]+(?=[0-9]{4,5}\s*.*)|[0-9]{2}(\-|\/)[0-9]{2}(\-|\/)[0-9]{4}\s*[0-9]{0,2}\s*[A-Z]+\s*[0-9]{0,2}\s*[A-Z\s]+(?=\s*[0-9]{1,2}\s*[0-9]{2,5})'
    # regexArray['FATHER NAME']=r'[a-zA-Z ]'
-    regexArray['ADDRESS']=r'[0-9]+\s+[a-zA-Z ]+\.+\n[a-zA-Z ]+[0-9]{5,8}'
+    regexArray['ADDRESS']=r'DOB\s*[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}\s*[0-9A-Z]{1,2}?\s*[A-Z]+.*\K[0-9]{4,5}\s*[A-Z]+(.*)[0-9]{5,6}\s*(?=[0-9]+\s*RE)|\K[0-9]{4,6}.*(?=CLASS)'
    # regexArray['INFO']=r'RESTRICTIONS\s+[a-zA-Z]+\sUEND\s+[a-zA-Z]+\n[0-9a-zA-Z]+\s+HGT\s[0-9]\/[0-9]{2}\s[0-9]{2}\s+[a-zA-Z]+\s(M|F)\s+[0-9]\.\s+[a-zA-Z]+\s[a-zA-Z]+'
     parsed_data = {}
     #for key, value in regexArray.iteritems() :
@@ -146,8 +146,8 @@ def ocr_default(img_file, preprocess) :
     for (attrib, regx) in regexArray.iteritems():
         print "Debug: ", attrib, ">> ", regx
     
-        if re.search(regx, newtest):
-            match = re.search(regx, newtest)
+        if regex.search(regx, newtest):
+            match = regex.search(regx, newtest)
             #print(key)
             #print "Match at index".(match.start(), match.end())
             string = (match.group(0))
