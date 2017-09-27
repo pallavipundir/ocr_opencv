1fbdeaa138c970c198fd44ff27d98eb44cf0a737
Pallavi Pundir <pallavi.pundir@biz2credit.org>
2017-09-27 10:36:45 +0530

    yo

--- libs/test.py
+++ libs/test.py
@@ -131,7 +131,7 @@ def ocr_default(img_file, preprocess) :
     #return text
     #print np.array(list(text))
     regexArray = {}
-    regexArray['MDL'] = r'DL\s*[0-9]{8,10}\s*(?=[0-9]+\s*.*)'
+    regexArray['DL'] = r'DL\s*[0-9]{8,10}\s*(?=[0-9]+\s*.*)'
     regexArray['CLASS']=r'CLASS+[a-zA-Z]'
     regexArray['ISS']=r'ISS\s+[0-9]{2}(\-|\/)[0-9]{2}(\-|\/)[0-9]{4}'
     regexArray['EXP']=r'EXP\s+[0-9]{2}(\-|\/)[0-9]{2}(\-|\/)[0-9]{4}'
