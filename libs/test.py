595481842097814325f8d6babc2696f55fde2a19
Pallavi Pundir <pallavi.pundir@biz2credit.org>
2017-09-27 19:16:46 +0530

    ed

--- libs/test.py
+++ libs/test.py
@@ -90,7 +90,7 @@ def ocr_default(img_file, preprocess) :
     
     text1=text.split() 
     newtest = " ".join(str(x) for x in text1)
-    return newtest
+    #return newtest
     #print("length is",len(text1))
     #For appending or store the results 
     #return newtest
@@ -136,9 +136,9 @@ def ocr_default(img_file, preprocess) :
     regexArray['ISS']=r'(I|L)SS\s*\K[0-9]{2}(\-|\/)[0-9]{2}(\-|\/)[0-9]{4}|ISSUED\s*\K[0-9]{2}(\-|\/)[0-9]{2}(\-|\/)([0-9]{4}|[0-9\s]{4,5})'
     regexArray['EXP']=r'(EXP|EXPIRES)\s*\K[0-9 ]{2,}(\-|\/|L)[0-9]{2}(\-|\/)[0-9]{4}'
     regexArray['DOB']=r'(DOB|DATE\s*OF\s*BIRTH|ONE)\s*\K[0-9]{2}(\-|\/)[0-9]{2}(\-|\/)[0-9]{4}'
-    regexArray['NAME']=r'(DOB|ONE)\s*[0-9]{2}(\/|\-)[0-9]{2}(\/|\-)[0-9]{4}\s*([0-9]{1,2})?\s*\K[A-Z]+\s*([A-Z]+)?\s*([0-9]{1,2}|\'|_)?\s*[A-Z ]+(?=\s*[0-9]{1,})|DRIVER\s*LICENSE\s*\K[A-Z\s]+(?=[0-9]{4,5}\s*.*)|[0-9]{2}(\-|\/)[0-9]{2}(\-|\/)[0-9]{4}\s*[0-9]{0,2}\s*[A-Z]+\s*[0-9]{0,2}\s*[A-Z\s]+(?=\s*[0-9]{1,2}\s*[0-9]{2,5})'
+    regexArray['NAME']=r'(DOB|ONE)\s*[0-9]{2}(\/|\-)[0-9]{2}(\/|\-)[0-9]{4}\s*([0-9]{1,2})?\s*\K[A-Z]+\s*([A-Z]+)?\s*([0-9]{1,2}|\'|_)?\s*[A-Z ]+(?=\s*[0-9]{1}\s*)|[0-9]{2}(\-|\/)[0-9]{2}(\-|\/)[0-9]{4}\s*[0-9]{0,2}\s*[A-Z]+\s*[0-9]{0,2}\s*[A-Z\s]+(?=\s*[0-9]{1,2}\s*[0-9]{2,5})|ARIZONA\s*DRIVER\s*LICENSE\s*\K[A-Z\s]+(?=[0-9]{1,5})'
    # regexArray['FATHER NAME']=r'[a-zA-Z ]'
-    regexArray['ADDRESS']=r'DOB\s*[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}\s*[0-9A-Z]{1,2}?\s*[A-Z]+.*\K[0-9]{4,5}\s*[A-Z]+(.*)[0-9]{5,6}\s*(?=[0-9]+\s*RE)|\K[0-9]{4,6}.*(?=CLASS)'
+    regexArray['ADDRESS']=r'(DOB|ONE)\s*[0-9]{2}(\/|\-)[0-9]{2}(\/|\-)[0-9]{4}\s*([0-9]{1,2})?\s*[A-Z]+\s*([A-Z]+)?\s*([0-9]{1,2}|\'|_)?\s*[A-Z ]+\s*[0-9]{1}\s*\K[0-9]{2,6}\s*[A-Z]+(.*)[0-9]{2,6}\s*(?=[0-9]+\s*RE)|\K[0-9]{2,6}.*(?=CLASS)'
    # regexArray['INFO']=r'RESTRICTIONS\s+[a-zA-Z]+\sUEND\s+[a-zA-Z]+\n[0-9a-zA-Z]+\s+HGT\s[0-9]\/[0-9]{2}\s[0-9]{2}\s+[a-zA-Z]+\s(M|F)\s+[0-9]\.\s+[a-zA-Z]+\s[a-zA-Z]+'
     parsed_data = {}
     #for key, value in regexArray.iteritems() :
@@ -209,4 +209,4 @@ def ocr_default(img_file, preprocess) :
     #return "\n".join(parsed_data).strip('"{}')
     #finalz=parsed_data.strip(' "{}')
     #finalz.split("\n")
-    #return '\n'.join("{}: {}".format(attrib, regx) for attrib, regx in parsed_data.items())
+    return '\n'.join("{}: {}".format(attrib, regx) for attrib, regx in parsed_data.items())
