060b0c47b40dd7636123d4d1c3e97ab237ff00b6
Pallavi Pundir <pallavi.pundir@biz2credit.org>
2017-10-16 13:02:20 +0530

    test

--- libs/test.py
+++ libs/test.py
@@ -92,7 +92,7 @@ def ocr_default(img_file, preprocess) :
     
     text1=text.split() 
     newtest = " ".join(str(x) for x in text1)
-    #return newtest
+    return newtest
     #print("length is",len(text1))
     #For appending or store the results 
     #return newtest
@@ -225,4 +225,4 @@ def ocr_default(img_file, preprocess) :
     #cv2.imshow("", img_file)
     #cv2.imshow("Person Identity", crop_img)
     #cv2.waitKey(0)
-    return finalz
+    #return finalz
