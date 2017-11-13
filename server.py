6032b84de877eac7b09d9ecceb610d2130d1d581
Pallavi Pundir <pallavi.pundir@biz2credit.org>
2017-11-13 11:42:10 +0530

    aadhary amn'

--- server.py
+++ server.py
@@ -4,7 +4,7 @@
 import os , datetime , logging
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug import secure_filename
-from libs.test import ocr_default
+from libs.aadhar import ocr_aadhar
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -33,7 +33,7 @@ def upload():
 
         files = app.config['UPLOAD_FOLDER']+'/'+filename
         #text = ocr_default(files,"thresh").encode('utf-8')
-        text = ocr_default(files,"thresh")
+        text = ocr_aadhar(files,"thresh")
         
         imgo = {
             'src': 'images/'+filename,
