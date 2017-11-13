a9e66ba16219c93f3b17ad282a0a595313f4c6fb
Pallavi Pundir <pallavi.pundir@biz2credit.org>
2017-11-13 13:37:35 +0530

    ohh bey

--- libs/same_link.py
+++ libs/same_link.py
@@ -87,7 +87,7 @@ def ocr_default(img_file, preprocess) :
     d= regex.search('(DL|MDL)\s*\K[A-Z0-9]{8,10}\s*|NUMBER\s*\K[A-Z0-9]{8,11}',newtest)
     if m:
         regexArray = dict.fromkeys(['NAME','DOB/YEAR OF BIRTH','AADHAAR NUMBER','GENDER'])
-        regexArray['NAME']=r'(WW R|INDIA EH F|[0-9]{9}|[0-9]{2,3}(::)?)(\s*)?\K[A-Z]+\s[A-Z]+|[A-Z ]+(?=WI\/)'
+        regexArray['NAME']=r'(WW R|MEW|WW R)(\s*)?\K[A-Z]+\s[A-Z]+|[A-Z ]+(?=WI\/)'
         regexArray['DOB/YEAR OF BIRTH']=r'[A-Z](\s*)?[0-9](\s*)?(\-|\/|I)[A-Z](\s*)?[0-9](\s*)?(\-|\/|I|L)[0-9]{4}|[0-9](\s*)?[0-9](\s*)?(\-|\/|I)[0-9](\s*)?[0-9](\s*)?(\-|\/|1)[0-9](\s*)?[0-9]{3}|(YEAR OF BIRTH)\s*(:)?\s*\K[0-9]+'
         regexArray['AADHAAR NUMBER']=r'[0-9 ]{15}'
         #regexArray['FATHER NAME']=r'[A-Z]{3,15}\s*[A-Z]+\s*([0-9](\s*)?[0-9](\s*)?(\-|\/|I)[0-9](\s*)?[0-9](\s*)?(\-|\/|I|L)[0-9]{4})|[A-Z]{3,15}\s*[A-Z]+\s*[0-9](\s*)?[0-9](\s*)?(\-|\/|I)[0-9](\s*)?[0-9](\s*)?(\-|\/|1)[0-9]\s*[0-9]{3}'    
