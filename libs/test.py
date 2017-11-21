454f19f4dd34d6594c0648fedab3e81229439852
pallavi pundir <pallavi.pundir18@outlook.com>
2017-11-21 18:18:11 +0530

    qwpotyerewwfwf

--- libs/test.py
+++ libs/test.py
@@ -113,10 +113,14 @@ def ocr_default(img_file, preprocess) :
     text = pytesseract.image_to_string(Image.open(filename)).upper()
    
     
-    text = regex.sub('[^a-zA-Z\s\d\w\b\t\n:\/.,*]*','', text)
+    #text = regex.sub('[^a-zA-Z\s\d\w\b\t\n:\/.]*','', text)
+    #text=re.sub('[^A-Za-z0-9\s*\n\t\b\d\/-]+', '', text)
     #return text
+    #‘->\ S / + ( «F! L 60 : 1 4 ' \ A. 2 C S ,..\. ® > S _ 
+    #text=re.sub(r'[?|$|.|!|/|\/|=|-|>|+|<<|@|_|,]',r'',text) 
     text1=text.split() 
-    newtest = " ".join(str(x) for x in text1)
+    newtest = " ".join(unicode(x) for x in text1)
+   
     return newtest
     #print("length is",len(text1))
     #For appending or store the results 
